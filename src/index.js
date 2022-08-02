26335cc040833bf4907e5be4015640565f42c192
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-02 15:38:24 -0500

    delete route login

--- src/index.js
+++ src/index.js
@@ -11,14 +11,12 @@ import "./index.css";
 /** View */
 import App from './App';
 import Dashboard from './routes/dashboard';
-import Login from './routes/login';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Login />} />
+      <Route path="/" element={<App />} />      
       <Route path="dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
