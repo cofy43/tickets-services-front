ade564887e589e4e3599553b864b0bd9fa5098ab
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-01 22:45:45 -0500

    update input component

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -9,7 +9,6 @@ const styleInput = {
   padding:"0px 22px",
   display:"block",
   maxWidth:"200px",
-  border:"none",
   backgroundColor: "rgb(234, 234, 234)",
   borderRadius: "4px",
   height: "50px"
@@ -23,15 +22,22 @@ const styleLabe = {
   position:"absolute",
   pointerEvents:"none",
   left:"5px",
-  top:"-20px",  
+  top:"-20px",
 }
 
 export default function CustomInput(props) {
   return (
-    <div style={styleGroup}>      
-      <input style={styleInput} type={props.type ? props.type : "text"} required={props.required}/>
+    <div style={styleGroup}>
+      <input
+        name={props.name}
+        onChange={props.onChange}
+        style={styleInput}
+        type={props.type ? props.type : "text"}
+        required={props.required}
+        pattern={props.pattern ? props.pattern : ""}
+      />
       <span className="bar"></span>
-      <label style={styleLabe}>{props.label}</label>
+      <label style={styleLabe}>{props.required ? "*" : ""}{props.label}</label>
     </div>
   );
 }
